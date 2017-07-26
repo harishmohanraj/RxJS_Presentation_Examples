59ec72a4e0e4bf3aaa16ea11eec6077311f87a3b
Harish Mohan Raj <hraj@sapient.com>
2017-07-26 15:35:45 +0100

    Other examples are removed.

--- AutoComplete/autoComplete_jquery.js
+++ AutoComplete/autoComplete_jquery.js
@@ -33,31 +33,3 @@
         $input.keyup(keyPressHandler);
     })
 })(jQuery, window)
-
-
-
-
-//
-//
-// function getWikipediaSearchResults(term) {
-//     return Rx.Observable.create((observer) => {
-//         var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + encodeURIComponent(term) + '&callback=?'
-//         $.getJSON(url, function(data) {
-//             observer.next(data[1]);
-//             observer.complete();
-//         })
-//     })
-// }
-//
-// var keyUp$ = Rx.Observable.fromEvent($input, "keyup");
-//
-// var searchTermStream$ = keyUp$
-//                         .debounceTime(250)
-//                         .map(e => e.target.value)
-//                         .filter(val => val.length > 2)
-//                         .distinctUntilChanged()
-//                         .switchMap(val => getWikipediaSearchResults(val));
-//
-// searchTermStream$.subscribe(
-//     (data) => $results.empty().append ($.map(data, function (v) { return $('<li>').text(v); }))
-// )
--- Iterator Pattern/simpleFunction.js
+++
@@ -1,7 +0,0 @@
-// Function Definition
-function add(a, b) {
-    return a+b;
-}
-// Calling the function
-add(1,1) //2
-
